bce3884708145fe9da7b7bb7b81078263ebfa0f6
Jeffrey Ruane <ruane.jb@gmail.com>
2016-01-13 20:30:31 -0700

    Starting to get hashed urls for polls

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const path = require('path');
 const socketIo = require('socket.io');
+const crypto = require('crypto');
 var poll = {};
 
 const bodyParser = require('body-parser');
@@ -16,9 +17,12 @@ app.get('/', function(req, res) {
 });
 
 app.post('/poll', function(req, res) {
-  poll = req.body;
-  res.sendFile(path.join(__dirname, 'public/poll-creation.html'));
-})
+  poll = req.body.poll;
+  var adminUrl = crypto.createHash('md5').update(poll.title + Date.now()).digest('hex');
+  var pollUrl = crypto.createHash('md5').update(poll.responses[0] + Date.now()).digest('hex');
+  console.log(req.body.title);
+  res.send("<div><a href=>'/" + adminUrl + "/" + pollUrl + "'>Admin URL</a><br><a href=>'/poll/" + pollUrl + "'>Poll URL</a>")
+});
 
 app.get('/admin/:id', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/admin.html'));
